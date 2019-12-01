083585498a866aad50cec96f0be6fa0b16efd7a6
Daniel Matras <daniel.matras@comarch.com>
2019-12-01 21:29:20 +0100

    solution day 1

---
+++ src/AdvOfCode2019/AdvOfCode2019/Day1/Solution1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace AdvOfCode2019.Day1
+{
+    public static class Solution1
+    {
+        public static void Day1() {
+
+            var input = File.ReadAllLines("Day1/input.in");
+
+            decimal fsum = 0;
+            decimal ssum = 0;
+            foreach (var i in input)
+            {
+                var mass = decimal.Parse(i);
+
+                var f = CountFuell(mass);
+                fsum += f;
+                do
+                {
+                    ssum += f;
+                    f = CountFuell(f);
+                } while (f >= 0);
+            }
+
+            Console.WriteLine("part one: " + fsum);
+            Console.WriteLine("part two: " + ssum);
+        }
+
+        private static decimal CountFuell(decimal mass) {
+            return Math.Floor(mass / 3.0m) - 2;
+        }
+    }
+}
---
+++ src/AdvOfCode2019/AdvOfCode2019/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdvOfCode2019.Day1;
+using System;
+
+namespace AdvOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution of day1:");
+            Solution1.Day1();
+        }
+    }
+}
